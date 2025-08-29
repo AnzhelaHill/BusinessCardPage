55a7ef80c9de7fdc3bcba4b44214caa2a1ccdb5a
Anzhela <gilevichanz@gmail.com>
2025-08-29 12:21:53 +0200

    Changed file 'mail.php'

--- mail.php
+++ mail.php
@@ -6,12 +6,12 @@ $subject = "Wiadomość z formularza na stronie";
 $to = "gilevichanz@gmail.com";
 $message = $_POST["msg"];
 
-$text = "Imię: " . $name . "\r\n" . "Email: " . $from . "\r\n" . "Treść: " . $message;
+$txt = "Imię: " . $name . "\r\n" . "Email: " . $from . "\r\n" . "Treść: " . $message;
 
 $headers = "From: " . $from . "\r\n";
 $headers .= "Reply-To: " . $from . "\r\n";
 
-$mail_status = mail($to, $subject, $text, $headers);
+$mail_status = mail($to, $subject, $txt, $headers);
 
 if($mail_status) {
     header('Location: index.html?mail_status=sent');
